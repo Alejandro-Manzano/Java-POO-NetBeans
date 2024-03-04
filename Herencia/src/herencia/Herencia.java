ca7e42354806cd3b3b070ee78fcead321547956e
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2024-03-04 17:09:09 +0100

    herencia empleado

--- Herencia/src/herencia/Herencia.java
+++ Herencia/src/herencia/Herencia.java
@@ -1,20 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package herencia;
 
-/**
- *
- * @author alexxml_
- */
 public class Herencia {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        System.out.println("ecefvc");
+        
+        Empleado emple = new Empleado();
+        
+        //método propio
+        emple.getNum_legajo();
+        //Método heredado de persona
+        emple.getNombre();
+        
+        Consultor consul = new Consultor();
+        consul.getNum_consultor();
+        consul.getApellido();
+        
     }
     
 }
